55bfd788acea9824969cf8efc5240abf7a6442f8
MattTheMicrowave <mwlluis@hotmail.com>
2016-06-01 14:06:49 -0500

    mongod

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var TaskModel = require('../models/TaskModel.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  TaskModel.find(function(err, tasks) {
+  	// asynchronous
+	  res.render('index', {
+	    title: 'Express',
+	    tasks: tasks
+  });
+    });
 });
 
+router.post('/tasks', function(req, res, next) {
+    var task = new TaskModel({
+    	  text : req.body.text,
+        version : req.body.version
+    });
+
+    task.save(function(err, task) {
+    	//asynch
+    res.redirect('/');
+});
+});
 module.exports = router;
